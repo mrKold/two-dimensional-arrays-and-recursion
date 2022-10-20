53c5c4d8c623958318e3f63a64c08a0926a12785
IvanK <mr.koldamasov@mail.ru>
2022-10-20 19:35:51 +0300

    Факториал через метод с Рекурсией

---
+++ Ex015/Program.cs
@@ -0,0 +1,17 @@
+﻿//Факториал через метод с Рекурсией
+
+
+double Factorial (int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if(n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+
+for(int i = 1; i<40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+
